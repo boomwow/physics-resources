f54a1a5f5fb453e0d0a11261f84274da6465a155
Dave Enrico <denrico@northbergen.k12.nj.us>
2017-02-15 10:43:59 -0500

    Fix data typing for np.array and use in-place operators for the update method

--- notebooks/python/ball.py
+++ notebooks/python/ball.py
@@ -2,12 +2,13 @@
 import numpy as np
 
 class Ball:
-    def __init__(self, mass=1, state=np.array([0, 0, 0, 0]), bounce=1):
+    # State var in the form [x, y, vx, vy]
+    def __init__(self, mass=1, state=np.array([0, 0, 0, 0], dtype=float), bounce=1):
         self.mass = mass
         self.state = state 
         self.bounce = bounce
 
     def update(self, dt):
-        self.state[3] = self.state[3] - 9.8 * dt
-        self.state[1] = self.state[1] + self.state[3] * dt
-
+        self.state[3] += -9.8 * dt 
+        self.state[1] += self.state[3] * dt
+        

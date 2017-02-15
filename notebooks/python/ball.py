d751bf4abbbdacb1daf9d55492a752087635381a
Dave Enrico <denrico@northbergen.k12.nj.us>
2017-02-15 10:04:31 -0500

    Ball class - update method

--- notebooks/python/ball.py
+++ notebooks/python/ball.py
@@ -2,7 +2,12 @@
 import numpy as np
 
 class Ball:
-    def __init__(self, mass, state=np.array([0, 0, 0, 0]):
+    def __init__(self, mass=1, state=np.array([0, 0, 0, 0]), bounce=1):
         self.mass = mass
-        self.state = np.array([x, y, vx, vy])
-        
+        self.state = state 
+        self.bounce = bounce
+
+    def update(self, dt):
+        self.state[3] = self.state[3] - 9.8 * dt
+        self.state[1] = self.state[1] + self.state[3] * dt
+

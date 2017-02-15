b0d9b55d2ec136efd3da42b30ab50b4bdfd5941b
Dave Enrico <denrico@northbergen.k12.nj.us>
2017-02-15 08:27:15 -0500

    Add sample ball class

---
+++ notebooks/python/ball.py
@@ -0,0 +1,8 @@
+# Sample Ball class
+import numpy as np
+
+class Ball:
+    def __init__(self, mass, state=np.array([0, 0, 0, 0]):
+        self.mass = mass
+        self.state = np.array([x, y, vx, vy])
+        

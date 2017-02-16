0057c5af493d51458e9dab917042a9a014ee061e
Dave Enrico <denrico@northbergen.k12.nj.us>
2017-02-16 09:51:31 -0500

    add test() to ball module

--- notebooks/python/ball.py
+++ notebooks/python/ball.py
@@ -1,19 +1,25 @@
 # Sample Ball class
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import animate
 
 class Ball:
     # State variable is in the form [x, y, vx, vy]
     def __init__(self, mass=1, state=np.array([0, 0, 0, 0], dtype=float), bounce=1):
         self.mass = mass
-        self.state = state 
+        self.state = state
         self.bounce = bounce
 
     def update(self, dt):
-        self.state[3] += -9.8 * dt 
+        self.state[3] += -9.8 * dt
         self.state[1] += self.state[3] * dt
-        if self.state[1] <= 0:
-            self.state[3] *= -1 * bounce
+        print(self.state)
 
 
+def test():
+
+    jim = Ball()
+    jim.state[1] = 1
+    
+    print(jim.state)
+    for i in range(20):
+        jim.update(.1)
